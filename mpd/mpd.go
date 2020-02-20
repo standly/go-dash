82a006840d729b006c96eec0cb016965a5321b07
standlee <standlee@foxmail.com>
2020-02-20 17:21:51 +0800

    add attr for test

--- mpd/mpd.go
+++ mpd/mpd.go
@@ -116,16 +116,17 @@ func (mpd *MPD) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 }
 
 type Period struct {
-	ID              string           `xml:"id,attr,omitempty"`
-	Duration        Duration         `xml:"duration,attr,omitempty"`
-	Start           *Duration        `xml:"start,attr,omitempty"`
-	BaseURL         string           `xml:"BaseURL,omitempty"`
-	SegmentBase     *SegmentBase     `xml:"SegmentBase,omitempty"`
+	ID          string       `xml:"id,attr,omitempty"`
+	Duration    Duration     `xml:"duration,attr,omitempty"`
+	Start       *Duration    `xml:"start,attr,omitempty"`
+	BaseURL     string       `xml:"BaseURL,omitempty"`
+	SegmentBase *SegmentBase `xml:"SegmentBase,omitempty"`
+
+	EventStream *EventStream `xml:"EventStream,omitempty"`
+
 	SegmentList     *SegmentList     `xml:"SegmentList,omitempty"`
 	SegmentTemplate *SegmentTemplate `xml:"SegmentTemplate,omitempty"`
 	AdaptationSets  []*AdaptationSet `xml:"AdaptationSet,omitempty"`
-
-	EventStream *EventStream `xml:"EventStream,omitempty"`
 }
 
 type DescriptorType struct {
